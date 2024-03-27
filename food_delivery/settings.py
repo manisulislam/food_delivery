7adfc807048e38158f1ed6f98426fdfcf08c2a20
anisul islam <anisulislam980340@gmail.com>
2024-03-28 00:55:33 +0600

    task deploy update

--- food_delivery/settings.py
+++ food_delivery/settings.py
@@ -1,5 +1,6 @@
 
-
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -65,11 +66,14 @@ WSGI_APPLICATION = 'food_delivery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
--- food_delivery/urls.py
+++ food_delivery/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path,include
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/delivery/', include('delivery_app.urls')),
 ]
+urlpatterns+=staticfiles_urlpatterns()
