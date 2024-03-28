cb41da851eafa7e1117eb8f00c9bbda354838239
anisul islam <anisulislam980340@gmail.com>
2024-03-28 11:16:32 +0600

    cors headers updated again

--- food_delivery/settings.py
+++ food_delivery/settings.py
@@ -36,9 +36,9 @@ CORS_ALLOW_ALL_ORIGINS=True
 CORS_ALLOWED_ORIGINS = [
    
     "http://localhost:8080",
-    "https://food-delivery-8yn6.onrender.com/"
+    "https://food-delivery-8yn6.onrender.com"
     "http://127.0.0.1:9000",
-    "http://localhost:5173/"
+    "http://localhost:5173"
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
